cc078fb32cbd19a4f9a468671d826a1614bda2ee
gokul-1998 <gokulakrishnanm1998@gmail.com>
2023-12-01 10:30:47 +0530

    Added versioning render

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
 import json
+import json
+import jsonpatch
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
@@ -49,8 +51,7 @@ class Widget(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     page_id = db.Column(db.Integer, db.ForeignKey('page.id',ondelete='Cascade'), nullable=False)
-    page = db.relationship('Page',cascade = "all,delete", backref=db.backref('widgets', lazy=True))
-
+    
     def __repr__(self):
         return '<Widget %r>' % self.id
     
@@ -67,6 +68,7 @@ class DashboardVersion(db.Model):
     
     dashboard = db.relationship('Dashboard',cascade = "all,delete", backref=db.backref('versions', lazy=True))
     version = db.Column(db.Integer, nullable=False)
+    difference_json= db.Column(db.JSON, nullable=True)
     created_at = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
@@ -75,13 +77,27 @@ class DashboardVersion(db.Model):
 with app.app_context():
   db.create_all()
 
+
+def apply_json_patch(original_json, json_patch):
+    # Apply JSON Patch to the original JSON
+    patched_json = jsonpatch.apply_patch(original_json, json_patch)
+    return patched_json
+
+def apply_patches(versions):
+    # print(versions)
+    resp=versions[0]
+    for version in versions[1:]:
+        resp=apply_json_patch(resp,version)
+        # print(resp)
+    return resp
+
 def add_dashboard(name, description):
     dashboard = Dashboard(name=name, description=description)
     db.session.add(dashboard)
     db.session.commit()
 
-def add_dashboard_version(dashboard_id, version, created_at):
-    dashboard_version = DashboardVersion(dashboard_id=dashboard_id, version=version, created_at=created_at)
+def add_dashboard_version(dashboard_id, version, created_at, difference_json={}):
+    dashboard_version = DashboardVersion(dashboard_id=dashboard_id, version=version, created_at=created_at, difference_json=difference_json)
     db.session.add(dashboard_version)
     db.session.commit()
 
@@ -94,6 +110,7 @@ def add_widget(name, page_id):
     widget = Widget(name=name, page_id=page_id)
     db.session.add(widget)
     db.session.commit()
+
 with app.app_context():  
   if Dashboard.query.count() == 0:
       add_dashboard('Dashboard 1', 'This is the first dashboard')
@@ -105,30 +122,93 @@ with app.app_context():
     #   add_widget('Widget 3', 2)
     #   add_widget('Widget 4', 2)
 
-def get_dashboard(id,filename):
+def get_dashboard_version(id,dashboard_id=1):
     resp={}
-    db_data= DashboardVersion.query.filter_by(id=id).first()
-    
-    temp=db_data.dashboard.get_dashboard()
-    resp['dashboard']=temp
-    resp['dashboard']['pages']=[page.get_page() for page in db_data.dashboard.pages]
-    for page in resp['dashboard']['pages']:
-        page['widgets']=[widget.get_widget() for widget in Widget.query.filter_by(page_id=page['id']).all()]
+    # to get the dashboard using the version passed in the url
+    all_versions=DashboardVersion.query.filter_by(dashboard_id=dashboard_id).filter(DashboardVersion.version>=id).order_by(DashboardVersion.version.desc()).all()
+
+
+    # all_versions=DashboardVersion.query.order_by(DashboardVersion.version.desc()).all()
+    # print(all_versions)
+    # print(id,all_versions[0].version)
+    # print(id,all_versions[1].version)
+    # print(type(id),type(all_versions[1].version))
+    if id==all_versions[0].version:
+        temp=all_versions[0].dashboard.get_dashboard()
+        resp['dashboard']=temp
+        resp['dashboard']['pages']=[page.get_page() for page in all_versions[0].dashboard.pages]
+        for page in resp['dashboard']['pages']:
+            page['widgets']=[widget.get_widget() for widget in Widget.query.filter_by(page_id=page['id']).all()]
+        return resp
+    if len(all_versions)==2:
+        if id==all_versions[1].version:
+            # print("in elif")
+            return all_versions[1].difference_json
+    elif len(all_versions)>2:
+        # resp={}
+        # temp=all_versions[1].difference_json
+        # for version in all_versions[1:]:
+        #     temp=apply_json_patch(temp,version.difference_json)
+        # return temp
+        lis=[version.difference_json for version in all_versions[1:]]
+        return apply_patches(lis)
+            
+        
     
 
-    print(resp)
-    # to write to json file
-    with open(f'versions/{filename}.json', 'w') as outfile:
-        json.dump(resp, outfile)
+
+    # db_data= DashboardVersion.query.filter_by(id=id).first()
+    
+    # temp=db_data.dashboard.get_dashboard()
+    # resp['dashboard']=temp
+    # #  to get the pages less than the version passed in the url
+    
+    # resp['dashboard']['pages']=[page.get_page() for page in db_data.dashboard.pages]
+    # for page in resp['dashboard']['pages']:
+    #     page['widgets']=[widget.get_widget() for widget in Widget.query.filter_by(page_id=page['id']).all()]
+    # return resp
 
 with app.app_context():
-    get_dashboard(1,"v2")
+    print(get_dashboard_version(1))
+import json
+import jsonpatch
+
+
 
 @app.route('/')
 def index():
-    db_version=DashboardVersion.query.filter_by(id=1).first()
-    print(db_version)
-    return render_template('index.html',db_version=db_version)
+    # the url will be /?dashboard_id=1&version=1
+    # db_version=DashboardVersion.query.filter_by(id=1).first()
+    dashboard_id=request.args.get('dashboard_id',1)
+    version=request.args.get('version','latest')
+    if version=="latest":
+        db_version=DashboardVersion.query.filter_by(dashboard_id=dashboard_id).order_by(DashboardVersion.version.desc()).first()
+        version=db_version.version
+    else:   
+        db_version=DashboardVersion.query.filter_by(dashboard_id=dashboard_id,version=int(version)).first()
+    print("db_version",version)
+    print("db_version",type(version))
+    dashboard_data=get_dashboard_version(int(version))
+    print("dashboard_data",dashboard_data)
+    # to order by descending order of  version till the version passed in the url
+    sub_versions_including_current=DashboardVersion.query.filter_by(dashboard_id=dashboard_id).filter(DashboardVersion.version>=version).order_by(DashboardVersion.version.desc()).all()
+
+    all_versions=DashboardVersion.query.filter_by(dashboard_id=dashboard_id).order_by(DashboardVersion.version.desc()).all()
+    all_versions_json=[version.difference_json for version in sub_versions_including_current]
+
+    # final=apply_patches(all_versions_json)
+    # print("final",final)
+    # if not final:
+
+    #     final=dashboard_data
+    # else:
+
+    #     final=(final)
+    #     final['dashboard']=dashboard_data['dashboard']
+    #     final['dashboard']['pages']=dashboard_data['dashboard']['pages']
+    #     for page in final['dashboard']['pages']:
+    #         page['widgets']=[widget for widget in page['widgets'] if widget['id'] in final['dashboard']['pages'][0]['widgets']]
+    return render_template('index.html',dashboard_data=dashboard_data,current_version=version,all_versions=all_versions)
 
 @app.route('/add_page/<int:dashboard_id>', methods=['GET', 'POST'])
 def add_page_route(dashboard_id):
@@ -177,18 +257,19 @@ def update_widget(id):
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('update_widget.html',widget=widget)
+
 import os
-@app.route("/versions")
-def get_all_versions():
-    version_files = os.listdir('versions')
-    versions = []
+# @app.route("/versions")
+# def get_all_versions():
+#     version_files = os.listdir('versions')
+#     versions = []
 
-    for filename in version_files:
-        with open(os.path.join('versions', filename)) as json_file:
-            data = json.load(json_file)
-            versions.append(data)
+#     for filename in version_files:
+#         with open(os.path.join('versions', filename)) as json_file:
+#             data = json.load(json_file)
+#             versions.append(data)
 
-    return render_template('version.html', versions=versions)
+#     return render_template('version.html', versions=versions)
 
 @app.route("/versions/<string:id>")
 def get_version(id):
@@ -222,6 +303,81 @@ def render_versioning():
 
     return render_template('versioning_template.html', versioning_data=versioning_data, selected_version=None)
 
+import json
+import jsonpatch
+
+def generate_json_patch(new_json, old_json):
+    # Generate JSON Patch
+    patch = jsonpatch.make_patch(new_json, old_json)
+    return json.loads(patch.to_string())
+def apply_json_patch(original_json, json_patch):
+    # Apply JSON Patch to the original JSON
+    patched_json = jsonpatch.apply_patch(original_json, json_patch)
+    return patched_json
+
+
+@app.route("/bla")
+def bla():
+    version=1
+    difference_json=json.loads(json.dumps(get_dashboard_version(1)))
+    print(difference_json)
+    print(type(difference_json))
+    # to jsonify the json
+    difference_json=json.dumps(difference_json)
+    print(type(difference_json))
+    # to add to the database as json
+    difference_json=json.loads(difference_json)
+    return difference_json
+           
+
+@app.route('/add_version/<int:dashboard_id>', methods=['GET', 'POST'])
+def add_version(dashboard_id):
+    # create a json file for the version
+    db_version=DashboardVersion.query.filter_by(dashboard_id=dashboard_id).order_by(DashboardVersion.version.desc()).all()[:2]
+    print("last two db_version",db_version)
+
+    if len(db_version)<=1:
+
+        for version in db_version:
+            print(f"get_dashboard_version {version.version}",get_dashboard_version(int(version.version)))
+            version.difference_json=json.loads(json.dumps(get_dashboard_version(int(version.version))))
+            print(version.difference_json)
+            print(type(version.difference_json))
+            
+            db.session.commit()
+            # return version.difference_json
+
+        
+    else:
+        print("inside else")
+        print(f"get version difference {db_version[0].version}",get_dashboard_version(db_version[0].version))
+        print(f"get version difference {db_version[1].version}",get_dashboard_version(db_version[1].version))
+        db_version[0].difference_json=get_dashboard_version(db_version[0].version)
+        db_version[1].difference_json=generate_json_patch(get_dashboard_version(db_version[0].version),get_dashboard_version(db_version[1].version))
+        db.session.commit()
+    # db_version.difference_json=json.dumps(db_version[0].difference_json)
+    version=db_version[0].version+1
+    add_dashboard_version(dashboard_id, version, datetime.now(),get_dashboard_version(version-1))
+    # db.session.commit()
+    return redirect(url_for('index'))
+    # get the dashboard data
+    # get the page data
+    # get the widget data
+    # write to json file
+    # add the version to the dashboard version table
+
+    # with open(f'versions/{filename}.json', 'w') as outfile:
+    #     json.dump(resp, outfile)
+
+    
+    # return redirect(url_for('index'))
+
+@app.route("/delete_version/<int:dashboard_id>/<int:version>")
+def delete_version(dashboard_id,version):
+    db_version=DashboardVersion.query.filter_by(dashboard_id=dashboard_id,version=version).first()
+    db.session.delete(db_version)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- x.py
+++ x.py
@@ -1,29 +1,24 @@
-v2 = {
-    "dashboard": {
-        "id": 1,
-        "name": "Dashboard 1",
-        "description": "This is the first dashboard",
-        "pages": [
-            {
-                "id": 1,
-                "name": "page 1",
-                "dashboard_id": 1,
-                "widgets": [
-                    {
-                        "id": 1,
-                        "name": "widget 1 edited",
-                        "page_id": 1
-                    },
-                    {
-                        "id": 2,
-                        "name": "widget 2",
-                        "page_id": 1
-                    }
-                ]
-            }
-        ]
-    }
-}
-
+v2={"dashboard": {"id": 1, "name": "Dashboard 1", "description": "This is the first dashboard", "pages": [{"id": 1, "name": "page 1", "dashboard_id": 1, "widgets": [{"id": 1, "name": "widget 1", "page_id": 1}]}]}}
+v1=[{"op": "add", "path": "/dashboard", "value": {"id": 1, "name": "Dashboard 1", "description": "This is the first dashboard", "pages": [{"id": 1, "name": "page 1", "dashboard_id": 1, "widgets": [{"id": 1, "name": "widget 1", "page_id": 1}]}]}}]
 import json
-print(json.dumps(v2, indent=2))
+import jsonpatch
+
+def apply_json_patch(original_json, json_patch):
+    # Apply JSON Patch to the original JSON
+    patched_json = jsonpatch.apply_patch(original_json, json_patch)
+    return patched_json
+# modified_json = apply_json_patch(v2, v1)
+# print("\nModified JSON after applying the patch:")
+# print(json.dumps(modified_json, indent=2))
+
+def apply_patches(versions):
+    print(versions)
+    resp=versions[0]
+    for version in versions[1:]:
+        resp=apply_json_patch(resp,version)
+        print(resp)
+    return resp
+
+x=apply_patches([v2,v1])
+# print(json.dumps(x, indent=2))
+
